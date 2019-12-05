258557c3dd2f4ceab7ef0cfd87672ff1a9502e22
BedinovOleksandr <alekzzandr@gmail.com>
2019-12-05 17:41:17 +0200

    add send_capcha function

--- bot_api.py
+++ bot_api.py
@@ -1,12 +1,16 @@
 import telebot
 
-mySlaveToken = 'your bot token'
+mySlaveToken = 'yor token here'
+bot = telebot.TeleBot(mySlaveToken)
 
 
 def notification(masage):
-    bot = telebot.TeleBot(mySlaveToken)
     bot.send_message(518356070, masage)
+
+def send_capcha():
+    bot.send_document(518356070, open('capcha.gif', 'rb'))
 
 
 if __name__ == '__main__':
-    notification('hello')
+    #notification('hello')
+    send_capcha()
