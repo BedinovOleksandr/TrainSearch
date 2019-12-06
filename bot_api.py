f6fb34bb7b5bd02753db13f6a43fd788e42152ef
BedinovOleksandr <alekzzandr@gmail.com>
2019-12-06 17:50:54 +0200

    added config module

--- bot_api.py
+++ bot_api.py
@@ -1,16 +1,26 @@
 import telebot
-
-mySlaveToken = 'yor token here'
-bot = telebot.TeleBot(mySlaveToken)
+from config import botToken
 
 
-def notification(masage):
-    bot.send_message(518356070, masage)
-
-def send_capcha():
-    bot.send_document(518356070, open('capcha.gif', 'rb'))
+bot = telebot.TeleBot(botToken)
 
 
-if __name__ == '__main__':
-    #notification('hello')
-    send_capcha()
+# def notification(masage):
+#     bot.send_message(518356070, masage)
+#
+#
+def send_capcha(chat_id):
+     bot.send_document(chat_id, open('capcha.gif', 'rb'))
+
+
+@bot.message_handler()
+def messege_bot(message):
+    print(message)
+    bot.send_message(message.chat.id, 'Привет')
+    send_capcha(message.chat.id)
+
+bot.infinity_polling()
+
+# if __name__ == '__main__':
+#     notification('hello')
+#     send_capcha()
